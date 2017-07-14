68f7a731113828b1139320b1e1f71858f64f14f1
yKicchan <yKicchanApp@gmail.com>
2017-07-14 21:01:53 +0900

    modified:[test.php]接続チェックの変更

--- docker/src/test.php
+++ docker/src/test.php
@@ -1,16 +1,9 @@
 <?php
-
 $mysqli  = new mysqli('mysql', 'user', 'password', 'database');
 
-$sql = "INSERT INTO `test` (`time`) VALUES ('".date('Y/m/d H:i:s')."')";
-$mysqli->query($sql);
-
-$sql = "SELECT * FROM `test`";
-if ($result = $mysqli->query($sql)) {
-    while ($row = $result->fetch_assoc()) {
-        echo "{$row['id']}: {$row['time']}<br>";
-    }
-    $result->close();
+if ($mysqli->connect_errno) {
+    printf("Failed: %s\n", $mysqli->connect_error);
+    exit();
 }
-
+printf("Success!!");
 $mysqli->close();
